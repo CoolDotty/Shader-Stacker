0d3ec6987d9505fa0a0c6bcf14b832e5e8c546cf
KarlTheCool <k@rlphilli.ps>
2021-08-10 20:28:18 -0700

    fix(1) remove deprecated moppin res change thing

--- lib/SpriteStacking/scripts/SsGlobals.gd
+++ lib/SpriteStacking/scripts/SsGlobals.gd
@@ -7,8 +7,3 @@ var default_height;
 
 func _ready():
 	default_height = get_viewport().size.y;
-
-func _process(delta):
-	if not Engine.editor_hint:
-		# TODO: Perspective without the jank Moppin_ method
-		get_viewport().size.y = default_height * cam_pitch;
