ad43683c479e662c2ab7c5844d4be77ca5ff0ed2
KarlTheCool <k@rlphilli.ps>
2022-10-30 17:36:28 -0700

    add reset options for reset2d

--- addons/ShaderStacker/Reset2D/script.gd
+++ addons/ShaderStacker/Reset2D/script.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+@export_category("Reset2D")
+@export var reset_rotation: bool = true
+@export var reset_scale: bool = true
+
+
 
 func _ready():
 	var id = get_viewport().get_viewport_rid().get_id()
@@ -11,5 +16,7 @@ func _process(delta):
 	var cam_rot = (cam.global_rotation if cam else 1.0)
 	var squish = 1 / (cam.zoom.y if cam else 1.0)
 	
-	self.scale.y = squish
-	self.rotation = cam_rot
+	if reset_scale:
+		self.scale.y = squish
+	if reset_rotation:
+		self.rotation = cam_rot
