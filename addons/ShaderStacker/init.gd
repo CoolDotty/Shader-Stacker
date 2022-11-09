56fc1efc163d1849ef07d39772fc917e4bdd3a8b
KarlTheCool <k@rlphilli.ps>
2022-11-08 19:39:54 -0800

    rename script files so its easier in the editior

--- addons/ShaderStacker/init.gd
+++ addons/ShaderStacker/init.gd
@@ -6,15 +6,15 @@ extends EditorPlugin
 func _enter_tree():
 	add_custom_type(
 			"SpriteStack", "Node2D",
-			preload("res://addons/ShaderStacker/SpriteStack/script.gd"), 
+			preload("res://addons/ShaderStacker/SpriteStack/SpriteStack.gd"), 
 			preload("res://addons/ShaderStacker/SpriteStack/icon.svg"))
 	add_custom_type(
 			"StackCamera", "Camera2D",
-			preload("res://addons/ShaderStacker/StackCamera/script.gd"), 
+			preload("res://addons/ShaderStacker/StackCamera/StackCamera.gd"), 
 			preload("res://addons/ShaderStacker/StackCamera/icon.svg"))
 	add_custom_type(
 			"Reset2D", "Node2D",
-			preload("res://addons/ShaderStacker/Reset2D/script.gd"), 
+			preload("res://addons/ShaderStacker/Reset2D/Reset2D.gd"), 
 			preload("res://addons/ShaderStacker/Reset2D/icon.svg"))
 
 
