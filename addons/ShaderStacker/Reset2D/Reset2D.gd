630a47b053752790ae0dcd1ff867e6ad79af81e7
Karl Phillips <k@rlphilli.ps>
2022-11-25 05:20:48 +0000

    add z and reset_position to Reset2d

--- addons/ShaderStacker/Reset2D/Reset2D.gd
+++ addons/ShaderStacker/Reset2D/Reset2D.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+
 @export_category("Reset2D")
+@export var z: int = 0
+@export var reset_position: bool = true
 @export var reset_rotation: bool = true
 @export var reset_scale: bool = true
-
 
 
 func _ready():
@@ -16,7 +18,11 @@ func _process(delta):
 	var cam_rot = (cam.global_rotation if cam else 1.0)
 	var squish = 1 / (cam.zoom.y if cam else 1.0)
 	
+	var up_vector = Vector2(0, -1).rotated(cam_rot - global_rotation) * squish
+	
 	if reset_scale:
 		self.scale.y = squish
 	if reset_rotation:
 		self.rotation = cam_rot
+	if reset_position:
+		self.position = up_vector * z
